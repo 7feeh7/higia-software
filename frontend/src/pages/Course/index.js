a0346e89628a16cfe8a09b639c6f5a99b855bcb6
Felipe Pires <felipe.pires.soaresti@gmail.com>
2020-05-11 23:47:43 -0300

    Refatorando página de cadastro de cursos

--- frontend/src/pages/Course/index.js
+++ frontend/src/pages/Course/index.js
@@ -1,38 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import Table from '../../components/Table';
-import { Container, Grid, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper  } from '@material-ui/core';
-import api from '../../services/api';
+import React from 'react';
 
 export default function Course() {
-    const[courses, setCourses] = useState([]);
-
-    useEffect(() => {
-        api.get('course').then(response => { setCourses(response.data); });
-    }, []);
-
     return(
-        <Container component="main" maxWidth="xl">
-            <Grid container justify="center">
-                <Table courses={courses} />
-                {/* <TableContainer component={Paper}>
-                    <Table>
-                        <TableHead>
-                            <TableRow>
-                                <TableCell>Id</TableCell>
-                                <TableCell>Nome</TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {courses.map((course) => (
-                                <TableRow key={course.id}>
-                                    <TableCell>{course.id}</TableCell>
-                                    <TableCell>{course.name}</TableCell>
-                                </TableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-                </TableContainer> */}
-            </Grid>
-        </Container>
+        <h1>Cursos</h1>
     );
 }
